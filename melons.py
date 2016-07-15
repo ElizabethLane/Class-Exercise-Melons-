ce74a37d8610a59b4ecfc2af6ca0afac14cbf4bc
Hackbright Student <info@hackbrightacademy.com>
2016-07-14 17:56:30 -0700

    completed part 2 working on part 3

--- melons.py
+++ melons.py
@@ -1,19 +1,26 @@
 """This file should have our order classes in it."""
 
 class AbstractMelonOrder(object):
-    def __init__(self, species, qty, country_code):
+    def __init__(self, species, qty, country_code, passed_inspection):
         self.species = species
         self.qty = qty
         self.shipped = False
         self.order_type = None
-        self.tax = None
+        self.tax = 0
         self.country_code = country_code
+        self.passed_inspection = False
 
     def get_total(self):
         """Calculate price."""
-
         base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+        if self.species == "Christmas melons":
+            base_price = float(base_price) * 1.5
+
+        total = float(1 + self.tax) * float(self.qty * base_price)
+
+        if self.qty < 10 and self.country_code is not "USA":
+            total = float(total) + 3
+
         return total
 
     def mark_shipped(self):
@@ -42,3 +49,12 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+
+    def __init__(self, species, qty, passed_inspection):
+        super(GovernmentMelonOrder, self).__init__(species, qty, 'USA', passed_inspection)
+
+    def mark_inspection(self):
+        self.passed_inspection = True
+
