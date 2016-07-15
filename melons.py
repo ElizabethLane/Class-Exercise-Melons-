8b4bf1f7fe87f1568c27e1de8fa6730c764647ff
Hackbright Student <info@hackbrightacademy.com>
2016-07-14 17:17:50 -0700

    completed lab exercise melons part 1

---
+++ melons.py
@@ -0,0 +1,44 @@
+"""This file should have our order classes in it."""
+
+class AbstractMelonOrder(object):
+    def __init__(self, species, qty, country_code):
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = None
+        self.tax = None
+        self.country_code = country_code
+
+    def get_total(self):
+        """Calculate price."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+        return total
+
+    def mark_shipped(self):
+        """Set shipped to true."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder): 
+    """A domestic (in the US) melon order."""
+
+    def __init__(self, species, qty):
+        super(DomesticMelonOrder, self).__init__(species, qty, 'USA')
+        self.tax = 0.08
+    
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        super(InternationalMelonOrder, self).__init__(species, qty, country_code)
+        self.tax = 0.17
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
